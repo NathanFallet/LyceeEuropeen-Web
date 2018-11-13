846627c215a9fa03be6f3adc1c10afd2680c6f2e
Nathan Fallet <contact@nathanfallet.me>
2018-11-13 20:11:07 +0100

    Changes about github updates on config.php

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to clone in your current directory and replace the current website
+
+```php
+<?php
+`find . | xargs rm -rf`;
+`git clone https://github.com/NathanFallet/LyceeEuropeen-Web ./`;
+?>
+```
--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
+
+// URL de la racine du site, finissant par un /
 $url = 'http://localhost/lycee/';
+
+// Connexion à la bdd (remplacez les identifiants)
 try {
   $bdd = new PDO('mysql:host=127.0.0.1;dbname=lycee_europeen', 'lycee_europeen', 'lycee_europeen', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 } catch (Exception $e) {
