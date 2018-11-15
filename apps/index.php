e1466231addbef817bbfc385a784ca493ac6dd20
Nathan Fallet <contact@nathanfallet.me>
2018-11-15 20:56:29 +0100

    Apps - TODO

--- apps/index.php
+++ apps/index.php
@@ -1,7 +1,9 @@
 <?php
+require '../config.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+// TODO: Tester les methodes et retourner les donnees correspondantes
 if($data['method'] == 'getPosts'){
   $response = array(array('text' => 'My beautiful post loaded by PHP on API Server (c\'est ici que PHP va load depuis la BDD)'));
   echo json_encode($response);
