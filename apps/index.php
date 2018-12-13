bb523562d1215c4380c1ab487b792fc2808954a1
Nathan Fallet <contact@nathanfallet.me>
2018-12-13 12:35:24 +0100

    Bug correction

--- apps/index.php
+++ apps/index.php
@@ -4,15 +4,15 @@ require '../config.php';
 $data = json_decode(file_get_contents('php://input'), true);
 
 if($data['method'] == 'getArticles()'){
-  $sql = $bdd->prepare("SELECT * FROM articles ORDER BY publish DESC");
+  $sql = $bdd->query("SELECT * FROM articles ORDER BY publish DESC");
   $response = $sql->fetchAll();
   echo json_encode($response);
 }else if($data['method'] == 'getPages()'){
-  $sql = $bdd->prepare("SELECT * FROM pages");
+  $sql = $bdd->query("SELECT * FROM pages");
   $response = $sql->fetchAll();
   echo json_encode($response);
 }else if($data['method'] == 'getSlides()'){
-  $sql = $bdd->prepare("SELECT * FROM slides");
+  $sql = $bdd->query("SELECT * FROM slides");
   $response = $sql->fetchAll();
   echo json_encode($response);
 }
