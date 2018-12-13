e07942fc20146cdecfb30eba3b369f5643dacc53
Nathan Fallet <contact@nathanfallet.me>
2018-12-13 12:48:18 +0100

    String to boolean for success

--- apps/index.php
+++ apps/index.php
@@ -6,16 +6,16 @@ $data = json_decode(file_get_contents('php://input'), true);
 if($data['method'] == 'getArticles()'){
   $sql = $bdd->query("SELECT * FROM articles ORDER BY publish DESC");
   $response = $sql->fetchAll();
-  $response['success'] = 'true';
+  $response['success'] = true;
   echo json_encode($response);
 }else if($data['method'] == 'getPages()'){
   $sql = $bdd->query("SELECT * FROM pages");
   $response = $sql->fetchAll();
-  $response['success'] = 'true';
+  $response['success'] = true;
   echo json_encode($response);
 }else if($data['method'] == 'getSlides()'){
   $sql = $bdd->query("SELECT * FROM slides");
   $response = $sql->fetchAll();
-  $response['success'] = 'true';
+  $response['success'] = true;
   echo json_encode($response);
 }
